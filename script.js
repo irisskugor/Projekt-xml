40f5aed241603c06db7c0a0126b41242ac7e2d04
irisskugor <iskugor@tvz.hr>
2026-06-03 17:10:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function pretrazi() {
     let pojam = document.getElementById("pojam").value;
-    fetch("https://itunes.apple.com/search?term=" + pojam + "&entity=song")
+    fetch("https://itunes.apple.com/search?term=" + pojam + "&entity=song&limit=20")
+    .then(function(r) {
+        return r.json();
+    })
 
-    .then(r => r.json())
-    .then(podaci => {
+    .then(function(podaci) {
         let rezultat = document.getElementById("rezultati");
         rezultat.innerHTML = "";
-        podaci.results.forEach(pjesma => {
-            rezultat.innerHTML += "<div class='kartica'>" +  "<img src='" + pjesma.artworkUrl100 + "'>" +
-            "<div>" +  "<h3>" + pjesma.trackName + "</h3>" +
+        podaci.results.forEach(function(pjesma) {
+            let audio = "";
+            if (pjesma.previewUrl) {
+                audio = "<audio controls>" + "<source src='" + pjesma.previewUrl + "' type='audio/mpeg'>" + "</audio>";
+            }
+            rezultat.innerHTML +=
+            "<div class='kartica'>" + "<img src='" + pjesma.artworkUrl100 + "'>" +
+            "<div>" + "<h3>" + pjesma.trackName + "</h3>" +
             "<p>Izvođač: " + pjesma.artistName + "</p>" + "<p>Album: " + pjesma.collectionName + "</p>" +
-            "<audio controls src='" + pjesma.previewUrl + "'></audio>" + "</div>" +
+            audio + "</div>" +
             "</div>";
-
         });
-
     });
 }
